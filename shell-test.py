4ff490a91678eb39d1834b135162b36f9683f7d3
Kou <bybynov09@gmail.com>
2017-05-03 23:58:10 +0800

    modified

--- shell-test.py
+++ shell-test.py
@@ -2,17 +2,24 @@ import os
 import sys
 import hashlib
 
-def md5sum(filename):
-	with open(filename, 'rb') as file_object:
-		file_content = file_object.read()
-	file_md5 = hashlib.md5(file_content)
-	return file_md5
 
+def md5sum(filename):
+    with open(filename, 'rb') as file_object:
+        file_object.seek(0)
+        file_content = file_object.read()
+    file_md5 = hashlib.md5(file_content)
+    return file_md5
+
+print("Session begin.")
+update_times = 0
 former_md5 = md5sum("test.txt")
-# os.system(command)
+
 while True:
-	if former_md5 != md5sum("test.txt"):
-		os.system("git add test.txt")
-		os.system("git commit -m \"Data changed.\"")
-		os.system("git push origin master")
-		former_md5 = md5sum("test.txt")
+    if former_md5 != md5sum("test.txt"):
+        print("====== Updating data ======")
+        os.system("git add test.txt")
+        os.system("git commit -m \"Data changed.\"")
+        # os.system("git push origin master")
+        former_md5 = md5sum("test.txt")
+        update_times += 1
+        print("====== Data updated for {} times in this session. ======".format(update_times))
