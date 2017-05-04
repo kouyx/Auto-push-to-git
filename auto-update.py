4de96562539491afc574956c09b131573b6d2c39
Kou <bybynov09@gmail.com>
2017-05-04 12:59:17 +0800

    IO interface updated.

---
+++ auto-update.py
@@ -0,0 +1,49 @@
+import os
+import hashlib
+
+def hashsum(file_name, algorithm="md5"):
+    # read 8K of file content (as a chunk) at one time
+    def read_chunks(file_object):
+        file_object.seek(0)
+        chunk = file_object.read(8 * 1024)
+        while chunk:
+            yield chunk
+            chunk = file_object.read(8 * 1024)
+        else:
+            file_object.seek(0)
+    # select algorithm optionally; default is md5
+    if algorithm == "md5":
+        method = hashlib.md5()
+    elif algorithm == "sha1":
+        method = hashlib.sha1()
+    # read file
+    if isinstance(file_name, basestring) and os.path.exists(file_name):
+        with open(file_name, 'rb') as file_object:
+            for chunk in read_chunks(file_object):
+                method.update(chunk)
+    elif file_name.__class__.name__ in ["StringIO", "cStringIO"] \
+            or isinstance(file_name, file):
+        for chunk in read_chunks(file_name):
+            method.update(chunk)
+    else:
+        return ""
+    return method.hexdigest()
+
+update_times = 0
+print("Input the name of file (e.g. test.txt) to be updated automatically:\n")
+filename = raw_input()
+former_hash = hashsum(filename)
+print("Session begin.\n")
+print("Press Ctrl + C to terminate if needed.\n")
+
+while True:
+    if former_hash != hashsum(filename):
+        print("====== Updating data ======")
+        os.system("git add test.txt")
+        os.system("git commit -m \"Data changed.\"")
+        os.system("git push origin master")
+        former_hash = hashsum(filename)
+        update_times += 1
+        print("====== Data updated ====== for {} times in this session\n"
+              .format(update_times))
+        print("Press Ctrl + C to terminate if needed.\n")
