53c94b9f66db15191221d3eef60467a1f3318600
Kou <bybynov09@gmail.com>
2017-05-04 22:02:25 +0800

    Handle KeyboardInterrupt explictly.

--- auto-update.py
+++ auto-update.py
@@ -1,6 +1,7 @@
 import os
 import hashlib
 
+
 def hashsum(file_name, algorithm="md5"):
     # read 8K of file content (as a chunk) at one time
     def read_chunks(file_object):
@@ -11,6 +12,7 @@ def hashsum(file_name, algorithm="md5"):
             chunk = file_object.read(8 * 1024)
         else:
             file_object.seek(0)
+
     # select algorithm optionally; default is md5
     if algorithm == "md5":
         method = hashlib.md5()
@@ -29,21 +31,29 @@ def hashsum(file_name, algorithm="md5"):
         return ""
     return method.hexdigest()
 
+
 update_times = 0
-print("Input the name of file (e.g. test.txt) to be updated automatically:\n")
-filename = raw_input()
+filename = raw_input("\nInput the name of file (e.g. test.txt)"
+                     " to be updated automatically:\n")
 former_hash = hashsum(filename)
+print("************************************************************************")
 print("Session begin.\n")
 print("Press Ctrl + C to terminate if needed.\n")
 
-while True:
-    if former_hash != hashsum(filename):
-        print("====== Updating data ======")
-        os.system("git add {}".format(filename))
-        os.system("git commit -m \"Data changed.\"")
-        os.system("git push origin master")
-        former_hash = hashsum(filename)
-        update_times += 1
-        print("====== Data updated ====== for {} times in this session\n"
-              .format(update_times))
-        print("Press Ctrl + C to terminate if needed.\n")
+try:
+    while True:
+        if former_hash != hashsum(filename):
+            print("====== Updating data ======")
+            os.system("git add {}".format(filename))
+            os.system("git commit -m \"Data changed.\"")
+            # os.system("git push origin master")
+            former_hash = hashsum(filename)
+            update_times += 1
+            print("====== Data updated ====== for {} times in this session\n"
+                  .format(update_times))
+            print("Press Ctrl + C to terminate if needed.\n")
+except KeyboardInterrupt:
+    print("User interrupt; session terminated.")
+    print("************************************************************************")
+except:
+    print("Other exceptions occur.")
